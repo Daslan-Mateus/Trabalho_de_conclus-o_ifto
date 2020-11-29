38be18373214f22f090e157fe72b187b8fd983c1
Daslan-Mateus <70038813+Daslan-Mateus@users.noreply.github.com>
2020-11-29 11:12:38 -0300

    avacandoVI

---
+++ app/views/home/about_me.php
@@ -0,0 +1,23 @@
+<section class="page-section bg-primary text-white mb-0" id="about">
+    <div class="container">
+        <!-- About Section Heading-->
+        <h2 class="page-section-heading text-center text-uppercase text-white">Sobre</h2>
+        <!-- Icon Divider-->
+        <div class="divider-custom divider-light">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+        <!-- About Section Content-->
+        <div class="row">
+            <div class="col-lg-4 ml-auto">
+                <p class="lead">Olá, meu nome é Daslan Mateus, gosto de tecnologias e de está atualizado, amo
+                    programação.</p>
+            </div>
+            <div class="col-lg-4 mr-auto">
+                <p class="lead">Todos os dias busco está por dentro das novidades do mercado, trabalho com
+                    responsabilidade e flexibilidade, sou eletrotécnico e desenvolvedor web </p>
+            </div>
+        </div>
+    </div>
+</section>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -128,29 +128,9 @@
         </div>
     </section>
     <!-- About Section-->
-    <section class="page-section bg-primary text-white mb-0" id="about">
-        <div class="container">
-            <!-- About Section Heading-->
-            <h2 class="page-section-heading text-center text-uppercase text-white">Sobre</h2>
-            <!-- Icon Divider-->
-            <div class="divider-custom divider-light">
-                <div class="divider-custom-line"></div>
-                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                <div class="divider-custom-line"></div>
-            </div>
-            <!-- About Section Content-->
-            <div class="row">
-                <div class="col-lg-4 ml-auto">
-                    <p class="lead">Olá, meu nome é Daslan Mateus, gosto de tecnologias e de está atualizado, sou
-                        cristão e amo programação.</p>
-                </div>
-                <div class="col-lg-4 mr-auto">
-                    <p class="lead">Todos os dias busco está por dentro das novidades do mercado, trabalho com
-                        responsabilidade e flexibilidade,</p>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php 
+    $this->view('home/about_me');
+?>
     <!-- Contact Section-->
     <section class="page-section" id="contact">
         <div class="container">
